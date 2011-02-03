8bce2a6ced450bf1bc9b954b9b46cf4c15d3e62e
drubin <davidrub@gmail.com>
2011-02-03 11:40:36 +0200

    Changed xmonad to not use greedy views, Makes it auto switch screens bassed on mod+# awesome thanks jdq from #xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -181,7 +181,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- mod-shift-[1..9], Move client to workspace N
     [((m .|. modm, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
+        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
     ++
     -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
